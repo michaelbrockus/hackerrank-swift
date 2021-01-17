8cd7b749b26de662aabe23219d1cf2eef962658e
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-01-17 08:08:48 -0800

    Update main.swift

--- main.swift
+++ main.swift
@@ -8,7 +8,11 @@
 // main is where program execution starts
 func main()
 {
-    print("Lets do some HackerRank stuff")
+    // read integers line by line
+    var a = Int(readLine()!)!
+    var b = Int(readLine()!)!
+    
+    print(a + b)
 } // end of function main
 
 main()
